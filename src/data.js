049469d46af86a404354a7f89fde71cfc83f2cb6
kevas cole <colekevas@gmail.com>
2024-12-09 12:02:43 +0000

    created a separate folder for cocktail data

---
+++ src/data.js
@@ -0,0 +1,29 @@
+export const data = [
+  {
+    name: "Lychee Spritz",
+    description: "",
+    // img:
+    // ingridients:
+    // color:
+  },
+  {
+    name: "Mudslide",
+    description: "",
+    // img:
+  },
+  {
+    name: "Mulled Apple Juice",
+    description: "",
+    // img:
+  },
+  {
+    name: "Apple Sparkle",
+    description: "",
+    // img:
+  },
+  {
+    name: "",
+    description: "",
+    // img:
+  },
+];
