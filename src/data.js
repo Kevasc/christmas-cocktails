c58067bd59fa9addf688cf5e6c3a20194e2ad8e9
kevas cole <colekevas@gmail.com>
2024-12-09 18:27:14 +0000

    added more details for the drinks and separated into two different arrays

--- src/data.js
+++ src/data.js
@@ -1,29 +1,67 @@
-export const data = [
-  {
-    name: "Lychee Spritz",
-    description: "",
-    // img:
-    // ingridients:
-    // color:
-  },
-  {
-    name: "Mudslide",
-    description: "",
-    // img:
-  },
-  {
-    name: "Mulled Apple Juice",
-    description: "",
-    // img:
-  },
-  {
-    name: "Apple Sparkle",
-    description: "",
-    // img:
-  },
-  {
-    name: "",
-    description: "",
-    // img:
-  },
-];
+export const drinkList = {
+  cocktails: [
+    {
+      name: "Lychee Spritz",
+      description: "A refreshing and fizzy lychee-flavored cocktail",
+      colour: "#FFC0CB",
+      img: "",
+      ingredients: [
+        "Lychee juice",
+        "Prosecco",
+        "Soda water",
+        "Ice",
+        "Mint leaves",
+      ],
+    },
+    {
+      name: "Mudslide",
+      description:
+        "A rich and creamy cocktail with chocolate and coffee liqueur",
+      colour: "#5C4033",
+      img: "",
+      ingredients: [
+        "Vodka",
+        "Coffee liqueur",
+        "Irish cream",
+        "Chocolate syrup",
+        "Ice",
+      ],
+    },
+    {
+      name: "Baby Guinneis",
+      description: "A little sweet treat",
+      colour: "#964B00",
+      img: "",
+      ingredients: ["Kahlua", "Baileys"],
+    },
+  ],
+
+  mocktails: [
+    {
+      name: "Mulled Apple Juice",
+      description: "A warm and spiced non-alcoholic drink for cozy evenings",
+      colour: "#FFA500",
+      img: "",
+      ingredients: [
+        "Apple juice",
+        "Cinnamon sticks",
+        "Cloves",
+        "Star anise",
+        "Orange slices",
+      ],
+    },
+    {
+      name: "Apple Sparkle",
+      description: "A bubbly and crisp apple-based mocktail",
+      colour: "#F0E68C",
+      img: "",
+      ingredients: [
+        "Apple juice",
+        "Sparkling water",
+        "Lemon juice",
+        "Ice",
+        "Mint leaves",
+      ],
+    },
+  ],
+};
