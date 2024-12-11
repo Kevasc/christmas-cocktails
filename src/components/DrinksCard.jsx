f0082825b77a1853e8d4a0e998ee3b16e5d57ad6
kevas cole <colekevas@gmail.com>
2024-12-11 20:38:40 +0000

    changed the color and look of the menu

--- src/components/DrinksCard.jsx
+++ src/components/DrinksCard.jsx
@@ -1,27 +1,19 @@
 const DrinksCard = ({ drink }) => {
   return (
     <div>
-      <div className="bg-yellow-300 text-black">
-        <h2>{drink.name}</h2>
-        <h3 className="text-sm">{drink.description}</h3>
-        <p className="text-xs">{drink.ingredients.join(", ")}</p>
-      </div>
-      <div
-        className="flex justify-center items-center m-4"
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <div
-          style={{
-            backgroundColor: drink.colour,
-            height: "50px",
-            width: "50px",
-            borderRadius: "50%",
-          }}
-        ></div>
+      <div className="text-black border-slate-950  border-2">
+        <h2 className="p-3">{drink.name}</h2>
+        <h3 className="p-3 text-sm">{drink.description}</h3>
+        <p className="text-xs p-3">{drink.ingredients.join(", ")}</p>
+        <div className="flex items-center mt-2">
+          <div
+            style={{
+              backgroundColor: drink.colour,
+              height: "100%",
+              width: "100%",
+            }}
+          ></div>
+        </div>
       </div>
     </div>
   );
