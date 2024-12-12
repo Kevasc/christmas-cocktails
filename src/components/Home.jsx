4582ee9b0732ef264f960ad6ef92e31125cb767d
kevas cole <colekevas@gmail.com>
2024-12-12 11:17:46 +0000

    cleaned up code, added santa png to the bottom and reformatted

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 import { drinkList } from "../data";
-import DrinksCard from "./DrinksCard"; // Import DrinksCard component
-
+import DrinksCard from "./DrinksCard";
+import santa from "./santa.png";
 const Home = () => {
   const cocktailList = drinkList.cocktails.map((drink, index) => {
     return <DrinksCard key={index} drink={drink} />;
@@ -10,19 +10,21 @@ const Home = () => {
   });
 
   return (
-    <div className="grid justify-center w-fit min-h-screen text-center p-6 border-slate-950 m-3 border-2 bg-red-100 items-center font-mono text-3xl ">
-      <h1 className="text-8xl font-monsieurLaDouliseRegular text-sh p-3 pt-2">
+    <div className="grid justify-center text-center p-8  border-2 bg-red-100 items-center font-mono">
+      <h1 className="text-8xl font-medium font-monsieurLaDouliseRegular p-3 pt-2">
         Christmas Cocktails
       </h1>
-      <h2 className="text-sm underline text-black-500 p-3">
+      <p className="text-base text-red-700 underline text-black-500 p-3">
         Cocktails & Shots
-      </h2>
-      <div className=" bg-white"> {cocktailList}</div>
-      <h2 className="text-sm underline text-black-500 p-3">Mocktails</h2>
-      <div className="bg-white ">{mocktailsList}</div>
-      {/* <div className="bg-red-100">
+      </p>
+      {cocktailList}
+      <p className="text-base  text-red-700 underline text-black-500 p-3">
+        Mocktails
+      </p>
+      {mocktailsList}
+      <div className="flex justify-center h-48">
         <img src={santa} alt="Logo" />
-      </div> */}
+      </div>
     </div>
   );
 };
