a5ccb16d219be5d921ca752ceea4e2167f0e0525
kevas cole <colekevas@gmail.com>
2024-12-12 11:16:52 +0000

    took out unused rules, changed text colors and added in ingredients section

--- src/components/DrinksCard.jsx
+++ src/components/DrinksCard.jsx
@@ -1,20 +1,10 @@
 const DrinksCard = ({ drink }) => {
   return (
-    <div>
-      <div className="text-black border-slate-950  border-2">
-        <h2 className="p-3">{drink.name}</h2>
-        <h3 className="p-3 text-sm">{drink.description}</h3>
-        <p className="text-xs p-3">{drink.ingredients.join(", ")}</p>
-        <div className="flex items-center mt-2">
-          <div
-            style={{
-              backgroundColor: drink.colour,
-              height: "100%",
-              width: "100%",
-            }}
-          ></div>
-        </div>
-      </div>
+    <div className="my-4 bg-white border-slate-950 border-2">
+      <h2 className="p-3 text-2xl font-medium">{drink.name}</h2>
+      <p className="p-3 text-sm">{drink.description}</p>
+      <p className="text-sm pt-3 underline text-green-700 ">Contains:</p>
+      <p className="text-xs p-3">{drink.ingredients.join(", ")}</p>
     </div>
   );
 };
