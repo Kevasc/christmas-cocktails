d13d4ef6382ab381b2ded9983291ade1ff58b573
kevas cole <colekevas@gmail.com>
2024-12-12 11:14:39 +0000

    deleted breakpoints as not needed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,22 +2,6 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
-    screens: {
-      phone: "640px",
-      // => @media (min-width: 640px) { ... }
-
-      tablet: "768px",
-      // => @media (min-width: 768px) { ... }
-
-      laptop: "1024px",
-      // => @media (min-width: 1024px) { ... }
-
-      desktop: "1280px",
-      // => @media (min-width: 1280px) { ... }
-
-      "2xl": "1536px",
-      // => @media (min-width: 1536px) { ... }
-    },
     extend: {
       fontFamily: {
         yesevaOne: ["Yeseva One", "sans-serif"],
