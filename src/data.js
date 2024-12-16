5de4e5d72b6cb7a9e63a63a6148eef91ace4733f
kevas cole <colekevas@gmail.com>
2024-12-16 14:41:08 +0000

    took out image and color keys, edited descriptions.

--- src/data.js
+++ src/data.js
@@ -3,76 +3,55 @@ export const drinkList = {
     {
       name: "Lychee Spritz",
       description:
-        "A light and bubbly cocktail with the exotic sweetness of lychee, complemented by sparkling Prosecco and a refreshing hint of mint.",
-      colour: "#FFC0CB",
-      img: "",
-      ingredients: [
-        "Lychee juice",
-        "Prosecco",
-        "Soda water",
-        "Ice",
-        "Mint leaves",
-      ],
+        "A light and bubbly cocktail with the exotic sweetness of lychee, complemented by sparkling Prosecco and a refreshing hint of fresh mint.",
+      ingredients: ["Lychee juice", "Prosecco", "Soda water", "Fresh Mint"],
     },
     {
       name: "Mudslide",
       description:
         "Indulge in this decadent blend of vodka, coffee liqueur, and Irish cream, swirled with rich chocolate syrup for a dessert in a glass.",
-      colour: "#5C4033",
-      img: "",
       ingredients: [
         "Vodka",
         "Coffee liqueur",
         "Irish cream",
         "Chocolate syrup",
-        "Ice",
       ],
     },
     {
       name: "Baby Guinness",
       description:
         "A delightful mini cocktail resembling a pint of Guinness, with sweet layers of coffee liqueur and Baileys",
-      colour: "#964B00",
-      img: "",
       ingredients: ["Kahlua", "Baileys"],
     },
     {
       name: "Moscow Mule",
       description:
-        "A zesty and invigorating cocktail combining the kick of vodka with spicy ginger beer and a refreshing twist of lime and mint.",
-      colour: "#F0E68C",
-      img: "",
+        "A zesty and invigorating cocktail combining the kick of vodka with spicy ginger beer and a refreshing twist of lime and fresh mint.",
       ingredients: ["Vodka", "Ginger Beer", "Fresh Mint", "Lime"],
     },
     {
       name: "Hugo",
       description:
-        "A floral and bubbly delight with the fresh flavors of elderflower and mint, balanced by crisp Prosecco and a splash of gin.",
-      colour: "#F0E68C",
-      img: "",
+        "A floral and bubbly delight with the fresh flavors of elderflower and fresh mint, balanced by crisp Prosecco and a splash of gin.",
       ingredients: [
         "Gin",
         "Elderflower",
         "Prosecco",
         "Soda Water",
         "Lime",
-        "Mint",
+        "Fresh Mint",
       ],
     },
     {
       name: "Woo Woo",
       description:
         "A fruity and vibrant mix of vodka, cranberry juice, and peach schnapps, finished with a tangy lime kick.",
-      colour: "#F0E68C",
-      img: "",
       ingredients: ["Vodka", "Cranberry Juice", "Peach Schnapps", "Lime"],
     },
     {
       name: "Espresso Martini",
       description:
         "A bold and elegant cocktail with the rich flavors of espresso, vodka, and Kahlua, topped with a creamy foam and coffee bean garnish.",
-      colour: "#F0E68C",
-      img: "",
       ingredients: ["Vodka", "Coffee", "Kahlua", "Coffee Bean Garnish"],
     },
   ],
@@ -82,8 +61,6 @@ export const drinkList = {
       name: "Mulled Apple Juice",
       description:
         "A cozy and aromatic drink with spiced apple juice, hints of cinnamon, cloves, and orange peel, sweetened with a touch of honey.",
-      colour: "#FFA500",
-      img: "",
       ingredients: [
         "Apple juice",
         "Orange Peel",
@@ -96,14 +73,12 @@ export const drinkList = {
     {
       name: "Apple & Elderflower Sparkle",
       description:
-        "A crisp and refreshing mocktail featuring the delicate floral notes of elderflower, complemented by bubbly apple juice and mint.",
-      colour: "#F0E68C",
-      img: "",
+        "A crisp and refreshing mocktail featuring the delicate floral notes of elderflower, complemented by bubbly apple juice and fresh mint.",
       ingredients: [
         "Apple juice",
         "Sparkling water",
         "Elderflower",
-        "Mint leaves",
+        "Fresh Mint",
       ],
     },
   ],
