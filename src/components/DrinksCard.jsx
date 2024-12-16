308bcae38ece5766fa56a33036585b72a7adfa9d
kevas cole <colekevas@gmail.com>
2024-12-16 14:41:47 +0000

    changed text color to black

--- src/components/DrinksCard.jsx
+++ src/components/DrinksCard.jsx
@@ -3,7 +3,7 @@ const DrinksCard = ({ drink }) => {
     <div className="my-4 bg-white border-slate-950 border-2">
       <h2 className="p-3 text-2xl font-medium">{drink.name}</h2>
       <p className="p-3 text-sm">{drink.description}</p>
-      <p className="text-sm pt-3 underline text-green-900 ">Contains:</p>
+      <p className="text-sm pt-3 underline ">Contains:</p>
       <p className="text-xs p-3">{drink.ingredients.join(", ")}</p>
     </div>
   );
