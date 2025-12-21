a0233aa306c084b23f7ad08c9bc1065f9483ffc6
kevas cole <colekevas@gmail.com>
2025-12-21 22:52:15 +0000

    deleted unwanted drinks and replaced the descriptions with recipies

--- src/data.js
+++ src/data.js
@@ -1,15 +1,9 @@
 export const drinkList = {
   cocktails: [
-    // {
-    //   name: "Lychee Spritz",
-    //   description:
-    //     "A light and bubbly cocktail with the exotic sweetness of lychee, complemented by sparkling Prosecco and a refreshing hint of fresh mint.",
-    //   ingredients: ["Lychee juice", "Prosecco", "Soda water", "Fresh Mint"],
-    // },
     {
       name: "Mudslide",
       description:
-        "Indulge in this decadent blend of vodka, coffee liqueur, and Irish cream, swirled with rich chocolate syrup for a dessert in a glass.",
+        "1 shot vodka, 1 shot kahlua, 1 shot Baileys, 1 1/2 shot double cream. Shake with ice and serve in a choclate swirled glass",
       ingredients: [
         "Vodka",
         "Coffee liqueur",
@@ -18,68 +12,28 @@ export const drinkList = {
       ],
     },
     {
-      name: "Baby Guinness",
+      name: "Baby Guinness / Mint Baby Guinness",
       description:
-        "A delightful mini cocktail resembling a pint of Guinness, with sweet layers of coffee liqueur and Baileys",
+        "Layer one shot of baileys onto a shot of kahlua using a spoon. Take as a shot",
       ingredients: ["Kahlua", "Baileys"],
     },
     {
       name: "Moscow Mule",
       description:
-        "A zesty and invigorating cocktail combining the kick of vodka with spicy ginger beer and a refreshing twist of lime and fresh mint.",
+        "1 shot vodka, tope with ginger beer, add a sprig of mint, a lime wheel and some ice",
       ingredients: ["Vodka", "Ginger Beer", "Fresh Mint", "Lime"],
-    },
-    {
-      name: "Hugo",
-      description:
-        "A floral and bubbly delight with the fresh flavors of elderflower and fresh mint, balanced by crisp Prosecco and a splash of gin.",
-      ingredients: [
-        "Gin",
-        "Elderflower",
-        "Prosecco",
-        "Soda Water",
-        "Lime",
-        "Fresh Mint",
-      ],
     },
     {
       name: "Woo Woo",
       description:
-        "A fruity and vibrant mix of vodka, cranberry juice, and peach schnapps, finished with a tangy lime kick.",
+        "1 shot vodka, 1 shot peach schnapps, top with cranberry juice and a lime wheel over ice",
       ingredients: ["Vodka", "Cranberry Juice", "Peach Schnapps", "Lime"],
     },
     {
       name: "Espresso Martini",
       description:
-        "A bold and elegant cocktail with the rich flavors of espresso, vodka, and Kahlua, topped with a creamy foam and coffee bean garnish.",
+        "1 shot vodka, 2 shots kahlua, 2 shots espresso. Shake well with ice to create a micro foam and pour out using the sieve",
       ingredients: ["Vodka", "Coffee", "Kahlua", "Coffee Bean Garnish"],
     },
   ],
-
-  // mocktails: [
-  //   {
-  //     name: "Mulled Apple Juice",
-  //     description:
-  //       "A cozy and aromatic drink with spiced apple juice, hints of cinnamon, cloves, and orange peel, sweetened with a touch of honey.",
-  //     ingredients: [
-  //       "Apple juice",
-  //       "Orange Peel",
-  //       "Cinnamon sticks",
-  //       "Cloves",
-  //       "Orange Peel",
-  //       "Honey",
-  //     ],
-  //   },
-  //   {
-  //     name: "Apple & Elderflower Sparkle",
-  //     description:
-  //       "A crisp and refreshing mocktail featuring the delicate floral notes of elderflower, complemented by bubbly apple juice and fresh mint.",
-  //     ingredients: [
-  //       "Apple juice",
-  //       "Sparkling water",
-  //       "Elderflower",
-  //       "Fresh Mint",
-  //     ],
-  //   },
-  // ],
 };
