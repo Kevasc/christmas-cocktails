6e2826fa61d7840d2516f2e7700490c53394cd42
kevas cole <colekevas@gmail.com>
2025-12-21 22:53:52 +0000

    updated the colors, layout, added tailwind screenbreaks and updated fonts

--- src/components/DrinksCard.jsx
+++ src/components/DrinksCard.jsx
@@ -1,10 +1,31 @@
 const DrinksCard = ({ drink }) => {
   return (
-    <div className="my-4 bg-red-50 border-pink-200 border-2 rounded-2xl">
-      <h2 className="p-3 text-3xl font-germaniaOne">{drink.name}</h2>
-      <p className="p-3 text-sm">{drink.description}</p>
-      <p className="text-m pt-3 font-germaniaOne">Contains:</p>
-      <p className="text-xs p-3">{drink.ingredients.join(", ")}</p>
+    <div className="w-full max-w-xl backdrop-blur-md px-3 py-3 text-center text-zinc-100">
+      <div className="flex flex-col gap-2">
+        <h2 className="text-2xl font-bold font-body tracking-wide text-zinc-200 xl:text-3xl">
+          {drink.name}
+        </h2>
+        <p className="text-[10px] uppercase tracking-widest text-slate-950 font-bold dark:text-zinc-400 mt-1 font-body">
+          How to make
+        </p>
+        <p className=" leading-snug text-zinc-200 text-zinc-300 font-body">
+          {drink.description}
+        </p>
+
+        {/* <p className="text-[10px] uppercase tracking-widest text-zinc-500 text-zinc-400 mt-1 font-body">
+          Contains
+        </p> */}
+
+        {/* <p className="text-xs text-zinc-600 text-xl text-zinc-400 font-body">
+          {drink.ingredients.join(", ")}
+        </p> */}
+      </div>
+      {/* divider */}
+      <div className="flex items-center justify-center gap-1 mb-10 my-3 opacity-100">
+        <div className="h-px w-24 bg-slate-950" />
+        <span className="text-slate-950">✮</span>
+        <div className="h-px w-24 bg-slate-950" />
+      </div>
     </div>
   );
 };
